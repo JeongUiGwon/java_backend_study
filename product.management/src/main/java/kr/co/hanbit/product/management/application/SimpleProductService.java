08d1e15b9147e28c2ce3eb74e0ed8253e4cd8709
의권 정 <jek9412@naver.com>
2024-02-12 18:46:43 +0900

    p.357 studying

--- product.management/src/main/java/kr/co/hanbit/product/management/application/SimpleProductService.java
+++ product.management/src/main/java/kr/co/hanbit/product/management/application/SimpleProductService.java
@@ -3,20 +3,36 @@ package kr.co.hanbit.product.management.application;
 import kr.co.hanbit.product.management.domain.Product;
 import kr.co.hanbit.product.management.infrastructure.ListProductRepository;
 import kr.co.hanbit.product.management.presentation.ProductDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 @Service
 public class SimpleProductService {
     private ListProductRepository listProductRepository;
+    private ModelMapper modelMapper;
     @Autowired
-    SimpleProductService(ListProductRepository listProductRepository) {
+    SimpleProductService(ListProductRepository listProductRepository, ModelMapper modelMapper) {
         this.listProductRepository = listProductRepository;
+        this.modelMapper = modelMapper;
     }
     public ProductDto add(ProductDto productDto) {
-        Product product = ??;
+        // 1. ProductDto를 Product로 변환하는 코드
+        Product product = modelMapper.map(productDto, Product.class);
+        
+        // 2. 레포지토리를 호출하는 코드
         Product savedProduct = listProductRepository.add(product);
-        ProductDto savedProductDto = ??;
+        
+        // 3. Product를 ProductDto로 변환하는 코드
+        ProductDto savedProductDto = modelMapper.map(savedProduct, ProductDto.class);
+        
+        // 4. Dto를 반환하는 코드
         return savedProductDto;
+    }
+    public ProductDto findById(Long id) {
+        Product product = listProductRepository.findById(id);
+        ProductDto productDto = modelMapper.map(product, ProductDto.class);
+        return productDto;
     }
 }
