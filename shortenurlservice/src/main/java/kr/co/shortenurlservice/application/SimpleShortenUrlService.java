e90f1427b84f452cb83018c804b69982adbd2d46
Jeong UiGwon <jek9412@naver.com>
2024-02-18 11:31:50 +0900

    p.585 Studying

---
+++ shortenurlservice/src/main/java/kr/co/shortenurlservice/application/SimpleShortenUrlService.java
@@ -0,0 +1,31 @@
+package kr.co.shortenurlservice.application;
+
+import kr.co.shortenurlservice.domain.ShortenUrl;
+import kr.co.shortenurlservice.domain.ShortenUrlRepository;
+import kr.co.shortenurlservice.presentation.ShortenUrlCreateRequestDto;
+import kr.co.shortenurlservice.presentation.ShortenUrlCreateResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SimpleShortenUrlService {
+
+    private ShortenUrlRepository shortenUrlRepository;
+
+    @Autowired
+    SimpleShortenUrlService(ShortenUrlRepository shortenUrlRepository) {
+        this.shortenUrlRepository = shortenUrlRepository;
+    }
+
+    public ShortenUrlCreateResponseDto generateShortenUrl(
+            ShortenUrlCreateRequestDto shortenUrlCreateRequestDto
+    ) {
+        String originalUrl = shortenUrlCreateRequestDto.getOriginalUrl();
+        String shortenUrlKey = ShortenUrl.generateShortenUrlKey();
+
+        ShortenUrl shortenUrl = new ShortenUrl(originalUrl, shortenUrlKey);
+        shortenUrlRepository.saveShortenUrl(shortenUrl);
+
+        ShortenUrlCreateResponseDto shortenUrlCreateResponseDto = new ShortenUrlCreateResponseDto(shortenUrl);
+
+        return shortenUrlCreateResponseDto;
+    }
+}
